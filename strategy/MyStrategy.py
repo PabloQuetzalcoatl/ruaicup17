6bff339977937717eef6190bb3e4f0f718fa7917
Pablo Quetzalcoatl <pablo@pokemail.net>
2017-11-07 01:28:02 +0300

    empty strategy

--- strategy/MyStrategy.py
+++ strategy/MyStrategy.py
@@ -1,7 +1,26 @@
-LOG = False
+from model.ActionType import ActionType
+from model.Game import Game
+from model.Move import Move
+from model.Player import Player
+from model.World import World
 
 
+class MyStrategy:
+    def move(self, me: Player, world: World, game: Game, move: Move):
+        print('--------{}------------'.format(world.tick_index))
+        print(move.left)
+        print(move.top)
+        print(move.right)
+        print(move.bottom)
+        print(move.x)
+        print(move.y)
+        print(move.vehicle_type)
+        if world.tick_index == 0:
+            move.action = ActionType.CLEAR_AND_SELECT
+            move.right = world.width
+            move.bottom = world.height
 
-
-print("Hello RAIC 2017 v 0.0.1")
-# versions : 
+        if world.tick_index == 1:
+            move.action = ActionType.MOVE
+            move.x = world.width
+            move.y = world.height / 2.0
